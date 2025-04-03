788512d646cdaff8fdf0911fcc30f991eabe1b90
Sodre744 <ghsodre@gmail.com>
2025-04-03 11:39:13 -0300

    Update aplicativo.py

--- aplicativo.py
+++ aplicativo.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bot MDF rodando com sucesso!"
+     return "Bot MDF rodando com sucesso!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+     app.run(host='0.0.0.0', port=5000)
